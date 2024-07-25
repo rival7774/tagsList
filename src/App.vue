2d58d9bb82906cdbb4b82f86114499bcdb6105e5
sakitov <samir.kitov@retailrocket.io>
2024-07-25 19:09:56 +0400

    Создал проект

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,19 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <v-container>
+    <TagsList alignment="left" />
+    <TagsList alignment="left" class="half-width" />
+    <TagsList alignment="justify" class="half-width" />
+  </v-container>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import TagsList from './components/TagsList.vue';
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
-  }
-}
-</script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+  components: {
+    TagsList,
+  },
+};
+</script>
---
+++ src/components/TagsList.vue
@@ -0,0 +1,13 @@
+<script>
+export default {
+  name: 'TagsList'
+};
+</script>
+
+<template>
+
+</template>
+
+<style scoped>
+
+</style>
