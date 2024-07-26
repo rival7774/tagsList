e64fb43adef8ec354f1a408df43e2976d94ff422
sakitov <samir.kitov@retailrocket.io>
2024-07-26 23:34:14 +0400

    Предварительно завершил

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,8 @@
 <template>
   <v-container>
-    <TagsList alignment="left" />
-    <TagsList alignment="left" class="half-width" />
-    <TagsList alignment="justify" class="half-width" />
+    <TagsList :tags="tags" alignment="left"/>
+    <TagsList :tags="tags" alignment="left" class="half-width"/>
+    <TagsList :tags="tags" alignment="justify" class="half-width"/>
   </v-container>
 </template>
 
@@ -11,9 +11,35 @@ import TagsList from './components/TagsList.vue';
 
 export default {
   name: 'App',
-
   components: {
-    TagsList,
+    TagsList
   },
+  data() {
+    return {
+      tags: []
+    };
+  },
+  created() {
+    this.loadTags();
+  },
+  methods: {
+    async loadTags() {
+      try {
+        const response = await fetch('/tags.json');
+        if (!response.ok) {
+          throw new Error('Network response was not ok');
+        }
+        this.tags = await response.json();
+      } catch (error) {
+        console.error('Error loading tags:', error);
+      }
+    }
+  }
 };
 </script>
+
+<style scoped>
+.half-width {
+  width: 50%;
+}
+</style>
--- src/components/TagsList.vue
+++ src/components/TagsList.vue
@@ -1,13 +1,119 @@
+<template>
+  <div :class="['tags-list', alignmentClass]" ref="tagsList">
+    <div v-for="(tag, index) in visibleTags" :key="index" class="tags-list__item">
+      <v-icon>{{ tag.icon }}</v-icon>
+      <v-icon v-if="index < visibleTags.length - 1">mdi-circle-small</v-icon>
+    </div>
+  </div>
+</template>
+
 <script>
 export default {
-  name: 'TagsList'
+  name: 'TagsList',
+  props: {
+    tags: {
+      type: Array,
+      required: true
+    },
+    alignment: {
+      type: String,
+      default: 'left',
+      validator: value => ['left', 'justify'].includes(value)
+    }
+  },
+
+  data() {
+    return {
+      visibleTags: [],
+    };
+  },
+
+  computed: {
+    alignmentClass() {
+      return this.alignment === 'justify' ? 'tags-list--justify' : 'tags-list--left';
+    }
+  },
+
+  methods: {
+    updateVisibleTags() {
+      this.visibleTags = this.tags;
+
+      this.$nextTick(() => {
+
+        const wrapTags = this.$refs.tagsList;
+        const tagsListWidth = this.$refs.tagsList.clientWidth;
+        const tagItems = wrapTags.children;
+
+        let totalWidth = 0;
+        let countTags = 0;
+
+        for (let i = 0; i < this.tags.length; i++) {
+          const srtInnerItem = `<span class="tags-list__text">${this.tags[i].text}</span>`;
+
+          if (tagItems[i]) {
+            const text = tagItems[i].children[0];
+            if (text && text.classList.contains('tags-list__text')) {
+              text.remove();
+            }
+
+            tagItems[i].insertAdjacentHTML('afterbegin', srtInnerItem);
+
+            if (tagItems[i].offsetWidth + totalWidth <= tagsListWidth) {
+              countTags += 1;
+              totalWidth += tagItems[i].offsetWidth;
+            } else {
+              while (tagItems[i] && i <= this.tags.length) {
+                tagItems[i].remove();
+                i++;
+              }
+              break;
+            }
+          }
+        }
+
+        this.visibleTags = this.tags.slice(0, countTags);
+      });
+    },
+  },
+
+  mounted() {
+    this.updateVisibleTags();
+    window.addEventListener('resize', this.updateVisibleTags);
+  },
+
+  beforeDestroy() {
+    window.removeEventListener('resize', this.updateVisibleTags);
+  },
+
+  watch: {
+    tags() {
+      this.visibleTags = this.tags;
+      this.updateVisibleTags();
+    }
+  }
 };
 </script>
 
-<template>
+<style lang="scss">
+.tags-list {
+  display: flex;
+  overflow: hidden;
+  width: 100%;
 
-</template>
+  &__item {
+    padding: 0 4px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    min-width: fit-content;
+  }
 
-<style scoped>
+  &--left {
+    justify-content: flex-start;
+  }
 
+  &--justify {
+    justify-content: space-between;
+  }
+}
 </style>
